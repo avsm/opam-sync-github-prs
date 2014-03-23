330818d6154e38b21264de4f62e43a0c290981f8
Anil Madhavapeddy <anil@recoil.org>
2014-03-23 14:13:36 +0000

    add first cut at the syncher shell script.
    Example : https://github.com/ocaml/opam-repository/pull/1818

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -ex
+
+REPO=../auto-opam-repository
+UPSTREAM=git://github.com/ocaml/opam-repository
+GEN=`pwd`/generate.native
+PULL=`pwd`/create_pull.native
+V=4.02.0dev
+if [ ! -d ${REPO} ]; then
+  git clone git@github.com:bactrian/opam-repository ${REPO}
+fi
+
+BRANCH=sync-prs-`date +%s`
+HRDATE=`date +%c`
+cd $REPO
+git pull $UPSTREAM master
+git checkout -b $BRANCH
+
+rm -rf compilers/${V}/${V}+pr*
+$GEN
+git add compilers
+git commit -a -m 'Sync latest compiler pull requests'
+git push origin $BRANCH
+$PULL -h $BRANCH -m "The latest compiler pull requests for OCaml $V as of $HRDATE" -t "Sync OCaml compiler PRs" -b master -r opam-repository -u bactrian -x ocaml
