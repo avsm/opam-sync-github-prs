b0c10bd4dd0492ab03ebe1b3dd66e3956d044b51
Anil Madhavapeddy <anil@recoil.org>
2015-04-21 17:25:15 +0100

    fix compiler version

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 REPO=../auto-opam-repository
 UPSTREAM=git://github.com/ocaml/opam-repository
 GEN=`pwd`/generate.native
-PULL=`pwd`/create_pull.native
+PULL=`pwd`/create_pull_request.native
 V=4.03.0
 if [ ! -d ${REPO} ]; then
   git clone git@github.com:bactrian/opam-repository ${REPO}
@@ -16,9 +16,9 @@ git pull $UPSTREAM master
 git checkout -b $BRANCH
 
 rm -rf compilers/${V}/${V}+pr*
-$GEN
+$GEN -compiler-version ${V}
 git add compilers
-git commit -a -m 'Sync latest compiler pull requests'
+git commit -a -m 'Sync latest compiler pull requests' || true
 git push origin $BRANCH
 $PULL -h $BRANCH -m "The latest compiler pull requests for OCaml $V as of
 $HRDATE" -t "Sync OCaml compiler PRs" -b master -r opam-repository -u bactrian \
